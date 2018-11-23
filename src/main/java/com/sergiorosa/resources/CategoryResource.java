2b0985d04f7dc624e01e2dd78d5726548df2a47e
Sergio Rosa <scarlosjazz@gmail.com>
2018-11-23 11:46:48 -0500

    Testing first class - domain - Categorie

--- src/main/java/com/sergiorosa/resources/CategoryResource.java
+++ src/main/java/com/sergiorosa/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.sergiorosa.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sergiorosa.domain.Categorie;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST test working!";
+	public List<Categorie> list() { 
+		
+		Categorie cat1 = new Categorie(1, "Computer");
+		Categorie cat2 = new Categorie(2, "Printer 300A");
+		
+		List<Categorie> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		
+		return list;
 	}
 
 }
