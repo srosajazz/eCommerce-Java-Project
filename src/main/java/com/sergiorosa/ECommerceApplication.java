16e33b8fda903eb0a16d402a94c5f61dcce46ab5
Sergio Rosa <scarlosjazz@gmail.com>
2018-11-23 22:04:10 -0500

    City and State

--- src/main/java/com/sergiorosa/ECommerceApplication.java
+++ src/main/java/com/sergiorosa/ECommerceApplication.java
@@ -8,15 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sergiorosa.domain.Categorie;
+import com.sergiorosa.domain.City;
 import com.sergiorosa.domain.Product;
+import com.sergiorosa.domain.State;
 import com.sergiorosa.repositories.CategoryRepository;
+import com.sergiorosa.repositories.CityRepository;
 import com.sergiorosa.repositories.ProductRepository;
+import com.sergiorosa.repositories.StateRepository;
 
 @SpringBootApplication
 public class ECommerceApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
 	
 	@Autowired
 	private ProductRepository productRepository;
@@ -43,8 +53,26 @@ public class ECommerceApplication implements CommandLineRunner {
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
 		
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		State  sta1 = new State(null, "Boston");
+		State  sta2 = new State(null, "Broklyn");
+		
+		City c1 = new City(null, "Massachusetts", sta1);
+		City c2 = new City(null, "New York", sta2);
+		City c3 = new City(null, "California", sta2);
+		
+		sta1.getCities().addAll(Arrays.asList(c1));
+		sta2.getCities().addAll(Arrays.asList(c2,c3));
+		
+		stateRepository.saveAll(Arrays.asList(sta1, sta2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+		
 		
 	}
 }
