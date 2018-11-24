3d84fe006c3c7d833aaf865db23171b914d99939
Sergio Rosa <scarlosjazz@gmail.com>
2018-11-24 10:30:46 -0500

    Add Address/Client and Enums

---
+++ src/main/java/com/sergiorosa/domain/Client.java
@@ -0,0 +1,121 @@
+package com.sergiorosa.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.sergiorosa.domain.enums.ClientType;
+
+public class Client implements Serializable{
+	 
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private String cpfOrCnpj;
+	private Integer type;
+	
+	private List<Address> adresses = new ArrayList<>();
+	
+	private Set<String> phones = new HashSet<>();
+	
+	public Client() {
+		
+	}
+
+	public Client(Integer id, String name, String email, String cpfOrCnpj, ClientType type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpfOrCnpj = cpfOrCnpj;
+		this.type = type.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOrCnpj() {
+		return cpfOrCnpj;
+	}
+
+	public void setCpfOrCnpj(String cpfOrCnpj) {
+		this.cpfOrCnpj = cpfOrCnpj;
+	}
+
+	public ClientType getType() {
+		return ClientType.toEnum(type);
+	}
+
+	public void setType(ClientType type) {
+		this.type = type.getCod();
+	}
+
+	public List<Address> getAdresses() {
+		return adresses;
+	}
+
+	public void setAdresses(List<Address> adresses) {
+		this.adresses = adresses;
+	}
+
+	public Set<String> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(Set<String> phones) {
+		this.phones = phones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
